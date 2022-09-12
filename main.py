04a7908edf96622082e5a12f5dc5d1e9eb01258d
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-09-12 23:44:24 +0900

    現在時刻を取得してs3 uriに付与する使用へ変更。ログを保存するように変更。

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import json
 import os
-import subprocess
+from datetime import datetime, timedelta, timezone
 
 import awswrangler as wr
 import boto3
-
-params = json.load(
-    open('./arguments.json')
-)
-
-unload_select_query_file_directory = params['unload_select_query_file_directory']
-unload_select_query = open(
-    unload_select_query_file_directory, 'r'
-    ).read()
-unload_to_s3_uri = params['unload_to_s3_uri']
-share_exclusive_s3_uri = params['share_exclusive_s3_uri']
-transfer_target_s3_uri = params['transfer_target_s3_uri']
-workgroup = params['workgroup']
-s3_output = params['s3_output']
+import pandas as pd
 
 session = boto3.Session(
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
@@ -25,44 +12,67 @@ session = boto3.Session(
     region_name='ap-northeast-1'
 )
 
-unload_query = f"""
-UNLOAD( {unload_select_query} )
-TO '{unload_to_s3_uri}'
-WITH ( format = 'PARQUET')
-"""
+def get_time_of_now():
+    delta = timedelta(hours=9)
+    JST = timezone(delta, 'JST')
+    return datetime.now(JST)
+
+def datetime_to_bucket_string(time_data : datetime):
+    str_time_data = str(time_data)
+    replace_chars = " :"
+    for char in replace_chars:
+        str_time_data = str_time_data.replace(char, '.')
+    return str_time_data[:-6]
 
 def main():
-    # # UNLOADするuriが既に存在しているかを確認
-    # bucket_exist = wr.s3.does_object_exist(
-    #     path=unload_to_s3_uri,
-    #     boto3_session=session
-    # )
-    # if bucket_exist:
-    #     raise FileExistsError("指定したバケットは既に存在しています。")
-    # 先にすべてのバケットのデータを削除する
-    wr.s3.delete_objects(
-        path=unload_to_s3_uri,
-        boto3_session=session
+    # arguments.jsonのデータを読み込む
+    params = json.load(
+        open('./arguments.json')
     )
-    wr.s3.delete_objects(
-        path=share_exclusive_s3_uri,
-        boto3_session=session
-    )
-    wr.s3.delete_objects(
-        path=transfer_target_s3_uri,
-        boto3_session=session,
-        s3_additional_kwargs={
+    unload_select_query_file_directory = params['unload_select_query_file_directory']
+    unload_select_query = open(
+        unload_select_query_file_directory, 'r'
+        ).read()
+    unload_to_s3_uri = params['unload_to_s3_uri']
+    share_exclusive_s3_uri = params['share_exclusive_s3_uri']
+    transfer_target_s3_uri = params['transfer_target_s3_uri']
+    workgroup = params['workgroup']
+    s3_output = params['s3_output']
+    result_save_s3_uri = params['result_save_s3_uri']
+    # バケットのフォルダ名に現在時刻を付ける
+    now = get_time_of_now()
+    now_bucket_string = datetime_to_bucket_string(now)
+    unload_to_s3_uri += now_bucket_string + "/"
+    share_exclusive_s3_uri += now_bucket_string + "/"
+    transfer_target_s3_uri += now_bucket_string + "/"
+    result_save_s3_uri += now_bucket_string + ".json"
+    unload_query = f"""
+    UNLOAD( {unload_select_query} )
+    TO '{unload_to_s3_uri}'
+    WITH ( format = 'PARQUET')
+    """
+
+    # UNLOADするuriが既に存在しているかを確認
+    for uri in [unload_to_s3_uri, share_exclusive_s3_uri, transfer_target_s3_uri, result_save_s3_uri]:
+        bucket_exist = wr.s3.does_object_exist(
+            path=uri,
+            boto3_session=session,
+            s3_additional_kwargs={
             "ACL":"bucket-owner-full-control"
         }
-    )
+        )
+        if bucket_exist:
+            raise FileExistsError("指定したバケットは既に存在しています。")
+    
     # UNLOAD実行 unload_resultからはQueryExecutionIdを取得する
     unload_result = wr.athena.start_query_execution(
         unload_query,
         s3_output=s3_output,
         boto3_session=session,
+        workgroup=workgroup,
         wait=True
     )
-    print(unload_to_s3_uri)
+
     # 共有バケットへのコピーを実行
     unload_objects = wr.s3.list_objects(
         path=unload_to_s3_uri,
@@ -89,8 +99,20 @@ def main():
             "ACL":"bucket-owner-full-control"
         }
     )
-    print(unload_result)
-    print(share_bucket_copy_result)
-    print(transfer_bucket_copy_result)
+    # 実行結果を保存
+    result_dict = {
+        "datetime": str(now),
+        "unload_query_execudion_id" : unload_result["QueryExecutionId"],
+        "share_bucket_objects" : share_bucket_copy_result,
+        "transfer_bucket_objects" : transfer_bucket_copy_result
+    }
+    result_df = pd.DataFrame(result_dict)
+    save_result = wr.s3.to_json(
+        df=result_df,
+        path=result_save_s3_uri,
+        index=False,
+        boto3_session=session
+    )
+    print(f"share completed. log file were saved to {save_result}")
 if __name__ == "__main__":
     main()
